a234cf487c0f1a21b7ce7b83dc7bb95302073bb2
CtrlShiftAlt <37661764+CtrlShiftAlt@users.noreply.github.com>
2024-08-28 17:15:56 +0800

    submit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert(123)
+window.onresize = function() {
+    changeTitle();
+};
+
+function changeTitle() {
+    const width = window.innerWidth || document.documentElement.clientWidth;
+    let title;
+    if (width <= 600) {
+        title = "mobile";
+    } else  if (width <= 1024) {
+        title = "tablet";
+    } else {
+        title = "desktop";
+    }
+    document.title = title;
+}
+
+changeTitle();
